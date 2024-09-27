d50c047b26048b60f54ce2c78a37e50e1a5b89a4
Alex Naltu <naltuok@gmail.com>
2024-09-27 13:18:13 +0200

    updated the navbar with links and logo

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,33 @@
+import { navLinks } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileMenu from "./mobile-menu";
+import Wrapper from "../wrapper/wrapper";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white fixed w-full top-0 mb-24">
+      <Wrapper>
+        <nav className="flex items-center justify-between">
+          <Link href="/">
+            <Image src="/logo.svg" alt="logo" width={100} height={100} />
+          </Link>
+          <ul className="hidden sm:inline-flex gap-4">
+            {navLinks.map((link, i) => (
+              <li
+                key={i}
+                className="font-black hover:text-red-800 transition-all duration-200 ease-in-out md:text-xl"
+              >
+                <Link href={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <MobileMenu />
+        </nav>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Navbar;
