d59e9f8e6e5d653ca44e876d5e4a354f41d35fe3
Alex Naltu <naltuok@gmail.com>
2024-09-27 13:19:17 +0200

    a section for all the public projects

---
+++ src/components/scroll-sections/projects-section.tsx
@@ -0,0 +1,47 @@
+// ProjectsSection.tsx
+import { websites } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProjectsSection = () => {
+  return (
+    <section className="flex flex-col gap-3" id="#projects">
+      <h1 className="uppercase text-xl font-black md:text-2xl lg:text-4xl">
+        Projects
+      </h1>
+      {websites.map((project, i) => (
+        <div key={i} className="bg-black p-2 md:flex">
+          <div>
+            <h2 className="text-xl">{project.title}</h2>
+            <div className="bg-gradient-to-l from-black from-50% to-white to-100% w-full h-2" />
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={1000}
+              height={1000}
+              className="py-3 md:hidden"
+            />
+            <p className="md:mt-2">{project.description}</p>
+            <Link
+              href={project.url}
+              className="hidden md:block mt-3 font-black underline hover:text-red-800 transition-all duration-200 ease-in-out w-fit"
+              target="_blank"
+            >
+              Live Preview
+            </Link>
+          </div>
+          <Image
+            src={project.image}
+            alt={project.title}
+            width={1000}
+            height={1000}
+            className="hidden md:block  md:max-w-[40%] object-contain"
+          />
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default ProjectsSection;
